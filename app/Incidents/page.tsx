3d68702563db91ef0ef9e22b068aa12693afc4d7
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2024-04-17 10:16:09 +0300

    update test

--- app/Incidents/page.tsx
+++ app/Incidents/page.tsx
@@ -2,11 +2,7 @@ import { Incident } from "@/Libs/Types";
 import IncidentCard from "../_Components/IncidentCard";
 
 const FetchVerifiedReports = async () => {
-	const url: string | undefined = process.env.BASEAPIURL;
-	if (url === "" || url === undefined) {
-		throw new Error("Could not Link to database");
-	}
-	const res = await fetch(url + "/incidences", {
+	const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
 		cache: "reload",
 	});
 	
@@ -25,16 +21,11 @@ export default async function IncidentsPage() {
 	if (data)
 	{
 		return (
-			<>
+			
 				<div className='text-main w-full px-2 flex flex-col  md:flex-row md:flex-wrap  justify-center gap-4 py-6'>
-					{data?.map((incident) => (
-						<IncidentCard
-							key={incident._id}
-							Item={incident}
-						/>
-					))}
+					<p>Data was found</p>
 				</div>
-			</>
+			
 		);
 	}
 }
