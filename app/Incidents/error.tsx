eb91f5e6d678a09acc25c626ae302bc04921bcde
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2024-04-17 13:03:58 +0300

    Update error page

--- app/Incidents/error.tsx
+++ app/Incidents/error.tsx
@@ -1,8 +1,17 @@
-'use client' // Error components must be Client Components
+'use client'
+import Link from "next/link"
+
+ // Error components must be Client Components
 export default function Error() {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
+    <div className="p-4 text-main text-center flex flex-col items-center">
+    <h2 className="text-3xl font-bold mt-2">Something went wrong!</h2>
+    <p className="mt-3">Go back Home</p>
+    <Link className="p-4 bg-main text-white rounded w-[300px] hover:bg-opacity-40 my-4" href='/' >Home</Link>
+    <br />
+    <p  className="text-lg font-semibold">OR</p>
+    <p>Try again Later</p>
+    
     </div>
   )
 }
